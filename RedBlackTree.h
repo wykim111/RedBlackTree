9e0c3d2266c7646f559eda8c698ae18e4f3a30bc
WYKIM\ksq11 <ksq111@naver.com>
2022-08-21 02:59:16 +0900

    + 레드블랙트리 삽입 구현     - uncle 노드가 NULL인 경우 예외처리
    + 레드블랙트리 출력 결과 구현

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -14,6 +14,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define RED 0
 #define BLACK 1
@@ -31,6 +32,13 @@ typedef struct _red_black_treenode
 }Red_Black_TreeNode;
 
 Red_Black_TreeNode* RBT_Create_Node(int data);
+void RBT_InsertNode(Red_Black_TreeNode** tree, Red_Black_TreeNode* new_node);
+void RBT_InsertNodeHelper(Red_Black_TreeNode** tree, Red_Black_TreeNode* new_node);
+void RBT_RotateRight(Red_Black_TreeNode** root, Red_Black_TreeNode* parent);//rotate LL
+void RBT_RotateLeft(Red_Black_TreeNode** root, Red_Black_TreeNode* parent);//rotate RR
+void RBT_RebuildAfterInsert(Red_Black_TreeNode** root, Red_Black_TreeNode* cur_node);
+
+
 void show_menu();
 
 #endif
